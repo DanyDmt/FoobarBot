78adf8a7c31e755004027d7f9fc98ebb871a7bd8
DanyDmt <37958367+DanyDmt@users.noreply.github.com>
2018-04-01 19:12:53 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+/* Commandes pratique */
+client.on('message', message => {
+  if (message.content.startsWith('Salut')) {
+    message.reply("yo");
+    console.log('${client}')
+  }
+});
+client.on('message', message => {
+  if (message.content.startsWith('Tu vas bien ?')) {
+    message.reply("oui et toi ?");
+    console.log('${client}')
+  }
+});
+client.on('message', message => {
+  if (message.content.startsWith('Que fait tu ?')) {
+    message.reply("j'admire mon créateur. :smile:");
+    console.log('${client}')
+  }
+});
+client.on('message', message => {
+  if (message.content.startsWith('Version')) {
+    message.reply("je suis qu'une version 1.0.0, mais j'évolurait dans le temps.");
+    console.log('${client}')
+  }
+});
+
+client.on('ready', () => {
+   console.log('Connexion reussi!'); 
+});
+
+client.login('YOUR_TOKEN');
